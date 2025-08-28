a1f3e960be1f647502df12860b6b2100fb219074
sazzadm371701 <sazzadhosen3717@gmail.com>
2025-08-28 23:00:42 +0600

    In js file entried the initial data

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+// Service data
+const emergencyServices = [
+  {
+    name: "Anti-Corruption Helpline",
+    nameEn: "Police",
+    number: "999",
+    icon: "../assets/police.png",
+    category: "Emergency",
+  },
+  {
+    name: "Fire Service Number",
+    nameEn: "Fire Service",
+    number: "16163",
+    icon: "../assets/fire-service.png",
+    category: "Emergency",
+  },
+  {
+    name: "National Emergency Number",
+    nameEn: "Ambulance",
+    number: "1990",
+    icon: "../assets/ambulance.png",
+    category: "Medical",
+  },
+  {
+    name: "BRAC Helpline Number",
+    nameEn: "BRAC",
+    number: "16161",
+    icon: "../assets/brac.png",
+    category: "Medical",
+  },
+  {
+    name: "Bangladesh Railway Helpline ",
+    nameEn: "Bangladesh Railway",
+    number: "16784",
+    icon: "../assets/Bangladesh-Railway.png",
+    category: "Transport",
+  },
+  {
+    name: "National Emergency Number",
+    nameEn: "Emergency Services",
+    number: "999",
+    icon: "../assets/emergency.png",
+    category: "Emergency",
+  },
+];
 
